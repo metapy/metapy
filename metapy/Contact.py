80029b543e0fdb8bf4155e342a2dcd99fff844e4
Tim Cameron Ryan <dev@tim-ryan.com>
2011-04-05 22:57:56 -0400

    Adds metapy directory

---
+++ metapy/Contact.py
@@ -0,0 +1,5 @@
+class ContactBook(object):
+	def __init__(self):
+		self.contacts = []
+		self.contacts_by_name = {}
+		self.contacts_by_email = {}
---
+++ metapy/__init__.py
@@ -0,0 +1,7 @@
+# add all services to system path
+import sys
+sys.path.append("twitter")
+sys.path.append("facebook")
+sys.path.append("google")
+sys.path.append("mailserver")
+
