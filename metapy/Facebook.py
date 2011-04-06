f08ad9b28d6a7c3233c9e930613b3a7098d8e5a4
Jon McKay <jonathan.mckay@students.olin.edu>
2011-04-06 11:30:14 -0400

    Added facebook contact python files

---
+++ metapy/Facebook.py
@@ -0,0 +1,13 @@
+import facebook, pickle, metapy
+
+
+class FacebookPerson():
+		def __init_(self, name, idNum):
+			self.name = name
+			self.idNum = idNum
+		name = None;
+		idNum = None;
+		gender = None;
+		location = None;
+		birthday = None;
+		email = None;
