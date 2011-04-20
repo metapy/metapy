d7d83543134b377023ab0a8142da187e0278a7d6
Tim Cameron Ryan <dev@tim-ryan.com>
2011-04-20 00:44:40 -0400

    Photos

--- meta-photos.py
+++ meta-photos.py
@@ -1,4 +1,5 @@
 from metapy import Facebook, Twitter, Google
 
-print Facebook.get_latest_photos()
-print Google.get_latest_photos()
+photos = [] + Facebook.get_latest_photos() + Google.get_latest_photos()
+for photo in photos:
+	print "Photo: %s" % photo.source
